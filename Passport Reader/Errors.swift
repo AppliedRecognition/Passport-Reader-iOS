c57050de2896ddfac4ddaef81ff8205e2b9d725e
Jakub Dolejs <jakubdolejs@gmail.com>
2025-08-27 10:54:08 +0100

    Custom errors implementation

--- Passport Reader/Errors.swift
+++ Passport Reader/Errors.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum FaceDetectionError: LocalizedError {
+    case noFaceDetected
+    
+    var errorDescription: String? {
+        switch self {
+        case .noFaceDetected:
+            return NSLocalizedString("No face detected", comment: "")
+        }
+    }
+}
+
+enum ImageError: LocalizedError {
+    case imageConversionFailed
+    
+    var errorDescription: String? {
+        switch self {
+        case .imageConversionFailed:
+            return NSLocalizedString("Image conversion failed", comment: "")
+        }
+    }
+}
+
+enum FaceMatchingError: LocalizedError {
+    case missingSelfieFace
+    
+    var errorDescription: String? {
+        switch self {
+        case .missingSelfieFace:
+            return NSLocalizedString("Selfie face is missing", comment: "")
+        }
+    }
+}
